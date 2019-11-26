a8ef5e0671d625e13946fdd03412ff1cd030e8a0
maffey <matiusz98@gmail.com>
2019-11-26 19:44:52 +0100

    some refactoring

--- chromosome.py
+++ chromosome.py
@@ -20,18 +20,10 @@ class Chromosome:
         return int(self.binary, base=2)
 
     def swap_bit(self, index):
-        print("SWAP_BIT: Open.")
-        print("SWAP_BIT: Index: ", index)
-        print("SWAP_BIT: Current value: ", self.binary, " --- ", self.get_integer())
         if self.binary[index] == "0":
-            print("SWAP_BIT: The bit = 0, changing to 1...")
-            print("SWAP_BIT: ", self.binary[:index] + "1" + self.binary[index + 1:])
             self.binary = self.binary[:index] + "1" + self.binary[index + 1:]
         else:
-            print("SWAP_BIT: The bit = 1, changing to 0...")
-            print("SWAP_BIT: ", self.binary[:index] + "0" + self.binary[index + 1:])
             self.binary = self.binary[:index] + "0" + self.binary[index + 1:]
-        print("SWAP_BIT: Closed.")
 
     def mutate(self) -> None:
         """
@@ -39,11 +31,8 @@ class Chromosome:
 
         If we do, select random index of a bit for a chromosome in range <0, 4>
         """
-        print(f"About to mutate {self}...")
         if random.random() < Chromosome.mutation_parameter:
-            print("Mutation accepted.")
             self.swap_bit(random.randint(0, 4))
-            print("Mutation completed.")
 
     def cross(self, chromosome) -> None:
         """
@@ -57,20 +46,14 @@ class Chromosome:
         chromosome_dad(chromosome_mom, cross_parameter)
         """
         if random.random() < Chromosome.crossing_parameter:
-            print("Cross begin")
-            print(f"Crossing {id(self)} with {id(chromosome)}...")
             # Select L parameter (bit index)
             cross_point = random.randint(0, 3)
-            print(f"Cross point: {cross_point}")
             # Save the parts that won't be changed
             frozen_part_chromosome_one = self.binary[:cross_point + 1]
             frozen_part_chromosome_two = chromosome.binary[:cross_point + 1]
             # Save the parts to be crossed (swapped)
             crossing_part_chromosome_one = self.binary[cross_point + 1:]
             crossing_part_chromosome_two = chromosome.binary[cross_point + 1:]
-            print(f"Expected outcome:\n{frozen_part_chromosome_one}|{crossing_part_chromosome_two}")
-            print(f"{frozen_part_chromosome_two}|{crossing_part_chromosome_one}")
             # Perform the crossing
             self.binary = frozen_part_chromosome_one + crossing_part_chromosome_two
             chromosome.binary = frozen_part_chromosome_two + crossing_part_chromosome_one
-            print("Cross end")
--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from chromosome import Chromosome
 from function import Function
 
 
+# logging.basicConfig()
+
+
 # This might stay out of class.
 def initialize_chromosomes(number_of_chromosomes, start_range, end_range):
     list_of_chromosomes = []
@@ -31,65 +34,65 @@ def redistribute_chromosomes(chromosomes, chromosomes_values, number_of_chromoso
     return random.choices(population=chromosomes, weights=chromosomes_values, k=number_of_chromosomes)
 
 
+def plot_piechart(values, labels):
+    # Plot a tasty pie chart
+    plt.pie(values, autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.legend(labels)
+    plt.axis('equal')
+    plt.tight_layout()
+    plt.show()
+
+
+def single_epoch(function, chromosomes):
+    # Display all chromosomes, create a list of integer values of them and create labels list for pie chart.
+    chromosomes_integers = []  # Get values of chromos into a list
+    chromosome_labels = []  # Needed for our sweet cake!
+    # print("=== CHROMOSOMES STARTER PACK ===")
+    for i in range(len(chromosomes)):
+        int_form = chromosomes[i].get_integer()
+        chromosomes_integers.append(int_form)
+        chromosome_labels.append(f"Ch{i}: {int_form}")
+    # Get values and its sum
+    function_values = function.get_values_list(chromosomes_integers)
+
+    # Plot a tasty pie chart DEBUG
+    # plot_piechart(fun_values, chromos_labels)
+
+    redistributed_chromos = redistribute_chromosomes(chromosomes, function_values, 6)
+
+    # If there is any code deserving a Code of Shame trophy, it's this one below.
+    # Make copies of chromosomes to allow for smooth crossing and mutations.
+    temp_chromos = []
+    for chromo in redistributed_chromos:
+        chromo_copy = copy.copy(chromo)
+        temp_chromos.append(chromo_copy)
+    redistributed_chromos = temp_chromos
+
+    # Cross chromosomes
+    for i in range(0, len(redistributed_chromos), 2):
+        redistributed_chromos[i].cross(redistributed_chromos[i + 1])
+
+    # Mutate chromosomes
+    for chromo in redistributed_chromos:
+        chromo.mutate()
+
+    return redistributed_chromos
+
+
 # Initialize function
-fun = Function(5, 1, 7, 12)
+fun = Function(5, 1, 7, 12)  # Random values, test phase
 fun.display()
 
 # Initialize chromosomes
+# TODO: PRINT THIS SHIT OUT AT THE BEGINNING
 chromos = initialize_chromosomes(6, 1, 31)  # Default values, test phase
 
-# Display all chromosomes, create a list of integer values of them and create labels list for pie chart.
-args_of_chromos = []  # Get values of chromos into a list
-labels = []  # Needed for our sweet cake!
-i = 0  # I'm too focused on the algorithm itself to put it more nicely, figure it out later.
-for chromo in chromos:
-    chromo.display()
-    int_form = chromo.get_integer()
-    args_of_chromos.append(int_form)
-    labels.append(f"Ch{i}: {int_form}")
-    i += 1
+# MAIN LOOP
+for epoch in range(1000):
+    if epoch == 0:
+        result = single_epoch(fun, chromos)
+    result = single_epoch(fun, result)
 
-print("Chromos count: ", Chromosome.chromosome_count)
-
-# TODO: Put all that shit into functions. and a loop.
-# Get values and its sum
-fun_values = fun.get_values_list(args_of_chromos)
-fun_sum = fun.get_sum(args_of_chromos)
-print("Sum of values: ", fun_sum)
-
-# Plot a tasty pie chart
-plt.pie(fun_values, autopct='%1.1f%%', shadow=True, startangle=140)
-plt.legend(labels)
-plt.axis('equal')
-plt.tight_layout()
-plt.show()
-
-redistributed_chromos = redistribute_chromosomes(chromos, fun_values, 6)
-
-# If there is any code deserving a Code of Shame trophy, it's this one below.
-# Make copies of chromosomes to allow for smooth crossing and mutations.
-temp_chromos = []
-for chromo in redistributed_chromos:
-    chromo_copy = copy.copy(chromo)
-    temp_chromos.append(chromo_copy)
-redistributed_chromos = temp_chromos
-
-# Redistribution: OK
-print("REDISTRIBUTED CHROMOSOMES")
-for chromo in redistributed_chromos:
-    chromo.display()
-
-# Cross chromosomes
-
-for i in range(0, len(redistributed_chromos), 2):
-    redistributed_chromos[i].cross(redistributed_chromos[i + 1])
-
-print("CROSSED CHROMOSOMES")
-for chromo in redistributed_chromos:
-    chromo.display()
-
-# Mutate chromosomes
-print("MUTATED CHROMOSOMES")
-for chromo in redistributed_chromos:
-    chromo.mutate()
-    chromo.display()
+print("=== FINISHED CHROMOSOMES ===")
+for c in result:
+    c.display()
